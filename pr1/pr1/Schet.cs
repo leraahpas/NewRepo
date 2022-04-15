2504e3a3cd494bba89b5a55f054a6920ff4298d8
leraahpas <103631918+leraahpas@users.noreply.github.com>
2022-04-15 10:27:22 +0300

    Добавили в класс данные

--- pr1/pr1/Schet.cs
+++ pr1/pr1/Schet.cs
@@ -6,5 +6,32 @@ namespace pr1
 {
     class Schet
     {
+        private string name;
+        private int nomer;
+        private double symma;
+
+        public void Imput()
+        {
+            Console.WriteLine("Введите название: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите номер счета: ");
+            nomer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стоимость: ");
+            symma = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}  {1}  {2}", name, nomer, symma);
+        }
+
+        public Schet (string name, int nomer, double symma)
+        {
+            this.name = name;
+            this.nomer = nomer;
+            this.symma = symma;
+        }
+
+        public Schet (string name) : this(name, 18, 10)
+        {
+
+        }
+        //1
     }
 }
